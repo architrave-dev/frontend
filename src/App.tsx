59e53f2a7c103ea90beece9a63cbd5f0c91d771b
pglibrary80 <pglibrary80@gmail.com>
2024-06-19 17:13:46 +0900

    chore: cleanup css, main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
 
   return (
     <>
+      <header></header>
+      <main></main>
+      <footer></footer>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { GlobalStyle } from "./styles/globalStyle.ts";
+import { GlobalStyle } from "@styles/globalStyle.ts";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/themes.ts";
+import { theme } from "@styles/themes.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
       <GlobalStyle />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
