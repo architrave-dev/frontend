0c681c924e1c022de080e72f2da36674af670814
pglibrary80 <pglibrary80@gmail.com>
2024-06-19 16:15:18 +0900

    chore: remove unusedFile & create styleFolder

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    line-height: 1.5;
+    min-height: 100vh;
+  }
+  
+  img, picture, svg, video, canvas {
+    display: block;
+    max-width: 100%;
+  }
+
+  input, textarea, button, select {
+    font: inherit;
+  }
+  
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  #root, #__next {
+    isolation: isolate;
+  }
+`;
+
+export { GlobalStyle };
---
+++ src/styles/themes.ts
@@ -0,0 +1,42 @@
+const colors = {
+  mainText: "#1a1a1a",
+  subText: "#4d4d4d",
+  popover: "#252525",
+  caption: "#7d7d7d",
+  placeholder: "#b3b3b3",
+  white: "#ffffff",
+};
+const fontSize = {
+  sm: "20px",
+  md: "24px",
+  lg: "40px",
+  xlg: "72px",
+};
+const fontWeight = {
+  thin: 100,
+  extraLight: 200,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semiBold: 600,
+  bold: 700,
+  extraBold: 800,
+  black: 900,
+};
+const typography = {
+  mainTitle: `font-size: ${fontSize.xlg}; font-weight: ${fontWeight.semiBold}`,
+  projectTitle: `font-size: ${fontSize.lg}; font-weight: ${fontWeight.bold}`,
+  button: `font-size: ${fontSize.md}; font-weight: ${fontWeight.medium}`,
+  gnbTap: `font-size: ${fontSize.md}; font-weight: ${fontWeight.medium}`,
+  selectedButton: `font-size: ${fontSize.md}; font-weight: ${fontWeight.medium}; text-decoration: underline`,
+  list: `font-size: ${fontSize.sm}; font-weight: ${fontWeight.medium}`,
+  caption: `font-size: ${fontSize.sm}; font-weight: ${fontWeight.normal}`,
+  body: `font-size: ${fontSize.sm}; font-weight: ${fontWeight.normal}`,
+};
+const theme = {
+  colors,
+  fontSize,
+  fontWeight,
+  typography,
+};
+export { theme };
