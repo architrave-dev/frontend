6b55835a8bb2493c363c5cc6428895d346647fc3
pglibrary80 <pglibrary80@gmail.com>
2024-07-03 19:03:46 +0900

    design: shared image & button

--- src/shared/ui/ImageStack.tsx
+++ src/shared/ui/ImageStack.tsx
@@ -2,16 +2,33 @@ import { Container } from "@styles/Layouts";
 import React from "react";
 import styled from "styled-components";
 
-export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {}
+export interface ImageSectionProps {
+  children?: React.ReactNode;
+}
 
-export default function ImageStack({ children, ...props }: ImageProps) {
+function ImageSection({ children }: ImageSectionProps) {
+  return <StackedSection>{children}</StackedSection>;
+}
+
+export interface ImageHeaderProps {
+  props: {
+    mainTitle: string;
+    paragraph: string;
+  };
+}
+
+function ImageHeader({ props }: ImageHeaderProps) {
   return (
-    <StackedSection aria-label="sr-description">
-      <img {...props} />
-      {children}
-    </StackedSection>
+    <StyledImageHeader>
+      <StyledMainTitle>{props.mainTitle}</StyledMainTitle>
+      <StyledParagraph>{props.paragraph}</StyledParagraph>
+    </StyledImageHeader>
   );
 }
+
+export const ImageStackSection = Object.assign(ImageSection, {
+  ImageHeader,
+});
 
 const StackedSection = styled.section`
   ${Container}
@@ -23,4 +40,20 @@ const StackedSection = styled.section`
     grid-column: 1 / -1;
     grid-row: 1 / -1;
   }
+`;
+
+const StyledImageHeader = styled.header`
+  margin-inline-start: 12rem;
+  margin-block-start: 75vh;
+  color: ${(props) => props.theme.colors.white};
+  justify-self: start;
+  align-self: start;
+`;
+
+const StyledMainTitle = styled.h1`
+  font-size: ${(props) => props.theme.fontSize.lg};
+`;
+
+const StyledParagraph = styled.p`
+  font-size: ${(props) => props.theme.fontSize.md};
 `;
