7c0158c90cd0530d752153cbfaea01bedcfe40d0
pglibrary80 <pglibrary80@gmail.com>
2024-07-16 21:10:49 +0900

    feat: project-list-item

---
+++ src/entities/project-list/ui/projectItem.tsx
@@ -0,0 +1,40 @@
+import { MarginBottom } from "@styles/Layouts";
+import { TypoCSS } from "@styles/typoGuide";
+import styled from "styled-components";
+
+export interface ProjectItemProps {
+  thumbnailImage: string;
+  mainTitle: string;
+}
+
+export default function ProjectItem({
+  thumbnailImage,
+  mainTitle,
+}: ProjectItemProps) {
+  return (
+    <StyledProjectItem>
+      <StyledMainTitle>{mainTitle}</StyledMainTitle>
+      <StyledImage src={thumbnailImage} />
+    </StyledProjectItem>
+  );
+}
+
+const StyledProjectItem = styled.div`
+  ${MarginBottom}
+  display: grid;
+  grid-auto-flow: column;
+  align-items: end;
+  column-gap: 6rem;
+`;
+
+const StyledMainTitle = styled.h2`
+  ${TypoCSS.mainTitle}
+  line-height: 7.2rem;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  overflow: hidden;
+`;
