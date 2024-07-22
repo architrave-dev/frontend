5a55c7fd95ff77e90423caa469f3c669cfb5a3d1
pglibrary80 <pglibrary80@gmail.com>
2024-07-22 21:05:05 +0900

    chore: cleanup project entity

--- src/entities/project/api/useProjectDescription.tsx
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-
-export default function useProjectDescription() {
-  const queryResult = useQuery({ queryKey: [""], queryFn: () => {} });
-  console.log(queryResult);
-  return <div>useProjectDescription</div>;
-}
